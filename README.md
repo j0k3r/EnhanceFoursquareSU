be3fc4d079ebe6e8dae594114061a4add44479da
Jeremy <jeremy.benoist@gmail.com>
2013-10-11 00:21:25 +0200

    Add empty values from edit panel

--- README.md
+++ README.md
@@ -6,11 +6,20 @@
 
 ### Search
 
- * Convert Bing.com link to Google.com (for "(search the web)")
- * Add a link to Google Maps
- * Move both link above the venue name
+  * Convert Bing.com link to Google.com (for "(search the web)")
+  * Add a link to Google Maps
+  * Move both link above the venue name
 
 ![](http://f.cl.ly/items/0D0O3S0X381G101B1R3q/search-1.png)
+
+### Empty value from edit panel
+
+  * Parse all empty value from edit panel and display them above the venue name.
+    If there are more than 6 empty values, we don't list them
+
+![](http://f.cl.ly/items/0Q1s2G0y0G3r1f2K1h3y/4%20empty%20fields.png)
+
+![](http://f.cl.ly/items/3r2B1E2X0L223B2u242M/8%20empty%20fields.png)
 
 ## How to use
 
--- js/enhance-su.js
+++ js/enhance-su.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
+    function initializeEnhanceBlock () {
+        // avoid duplicate enhancement
+        if ($('#enhance-su-block').html()) {
+            return;
+        }
 
+        $('#overlayHeader div.venueInfo.hasRedMarker').prepend('<p id="enhance-su-block"></p>');
+    }
+
+    /**
+     * Enhance search
+     *
+     * - Convert Bing.com link to Google.com (for "(search the web)")
+     * - Add a link to Google Maps
+     * - Move both link above the venue name
+     */
     function enhanceSearch () {
         // avoid duplicate enhancement
-        if ($('#enhance-su-google-maps').html()) {
+        if ($('#enhance-su-search').html()) {
             return;
         }
 
@@ -21,11 +36,49 @@ $(document).ready(function() {
 
         searchMaps = '<a target="_blank" href="' + hrefMaps + '">Google Maps</a>';
 
-        $('#overlayHeader div.venueInfo.hasRedMarker').prepend('<p id="enhance-su-google-maps">Search on: ' + searchGoogle + ' - ' + searchMaps + '</p>');
+        $('#enhance-su-block').append('<span id="enhance-su-search">Search on: ' + searchGoogle + ' - ' + searchMaps + '</span>');
+    }
+
+    /**
+     * Display empty values from edit panel
+     * to be display right now, instead of wasting one click
+     *
+     * - Parse all empty value from edit panel and display them above the venue name
+     *   If there are more than 6 empty values, we don't list them
+     */
+    function displayEmptyValue () {
+        // if edit panel doesn't exists or enhancement already exists
+        if (!$('div.editPanes div.editPane').html() || $('#enhance-su-edit-location').html()) {
+            return;
+        }
+
+        // loop through all value from the edit panel and find those with empty value
+        var emptyValues = [];
+        $('li.field.simpleField :input').each(function( index ) {
+            curItem = $(this);
+            if ('' === curItem.val()) {
+                emptyValues.push(curItem.parent().prev('div').html());
+            }
+        });
+
+        if (0 === emptyValues.length) {
+            return;
+        }
+
+        var text = '<span id="enhance-su-edit-location"><b>' + emptyValues.length + '</b> empty fields: ' + emptyValues.join(', ') + '</span>';
+
+        // display a more visible message if there is a lot of empty fields
+        if (6 < emptyValues.length) {
+            text = '<span id="enhance-su-edit-location"><b>' + emptyValues.length + ' empty fields !</b></span>';
+        }
+
+        $('#enhance-su-block').append(text);
     }
 
     // be sure that every new venue will be updated
     setInterval(function() {
+        initializeEnhanceBlock();
         enhanceSearch();
+        displayEmptyValue();
     }, 500);
 });
