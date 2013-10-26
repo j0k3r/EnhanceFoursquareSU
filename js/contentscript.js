1c35f66cc7e3264890d3683014fe0cb4fc2a16c6
Jeremy <jeremy.benoist@gmail.com>
2013-10-26 13:34:43 +0200

    Change the way to inject script
    This way, it is directly injected into the page. So I can access window.fourSq and make some api request to Foursquare using their token.

---
+++ js/contentscript.js
@@ -0,0 +1,5 @@
+// dirty hack: http://stackoverflow.com/a/9517879/569101
+var s = document.createElement('script');
+s.src = chrome.extension.getURL("js/enhance-su.js");
+(document.head||document.documentElement).appendChild(s);
+s.parentNode.removeChild(s);
